01b88ba34f8101f41da0db3f44d1ea8bf0e6bcbd
AmirabbasHamdipour <mhman884@gmail.com>
2026-02-15 20:22:37 +0330

    Update settings.gradle.kts

--- android/settings.gradle.kts
+++ android/settings.gradle.kts
@@ -24,3 +24,20 @@ plugins {
 }
 
 include(":app")
+
+// اضافه کردن namespace به ffmpeg_kit_flutter_min_gpl
+gradle.beforeSettings { settings ->
+    def ffmpegPath = settings.settings.rootDir.path + '/../.pub-cache/hosted/pub.dev/ffmpeg_kit_flutter_min_gpl-5.1.0/android/build.gradle'
+    def ffmpegFile = new File(ffmpegPath)
+    if (ffmpegFile.exists()) {
+        def content = ffmpegFile.text
+        if (!content.contains('namespace')) {
+            def newContent = content.replace(
+                "android {",
+                "android {\n    namespace 'com.arthenica.ffmpegkit.flutter.min_gpl'"
+            )
+            ffmpegFile.text = newContent
+            println "Namespace added to ffmpeg_kit_flutter_min_gpl"
+        }
+    }
+}
